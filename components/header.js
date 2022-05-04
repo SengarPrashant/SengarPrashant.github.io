12d716dbb0e6b0777fe6d338e16df415faa60a87
prashant89m <42473921+prashant89m@users.noreply.github.com>
2022-05-04 18:06:08 +0530

    fixed header
    fixed header

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 class Header extends HTMLElement {
     connectedCallback() {
-      this.innerHTML=`<nav class="navbar navbar-expand-lg navbar-light bg-light">
+      this.innerHTML=`<nav style="border-bottom:1px solid lightgray;" 
+      class="navbar navbar-expand-lg shadow-sm fixed-top navbar-light bg-light">
       <div class="container">
         <a class="navbar-brand" href="index.html">PS</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
